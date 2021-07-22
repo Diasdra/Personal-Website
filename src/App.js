141f1bc18d9234e0e6295519df35ceacb78f8b9c
Diasdra <sayrial@gmail.com>
2021-07-22 04:00:22 -0700

    changed Navbar style and switched to Styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar.js';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar/';
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
   return (
+    <Router>
       <Navbar />
+    </Router>
   );
 }
 
